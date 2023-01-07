b95fe4f1e6b696c5ba189d9af7d95081504d5bc7
miguel <miguel@cinaq.com>
2023-01-07 15:32:47 +0100

    created homepage hero box

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,9 @@
 import { CssBaseline, Typography } from '@mui/material';
 import {Button, AppBar, Toolbar} from "@mui/material";
 import { makeStyles } from '@material-ui/styles';
-import React from 'react'
+import React from 'react';
+import city from "./assets/city-hero.png"
+import zIndex from '@mui/material/styles/zIndex';
 
 const useStyle = makeStyles({
     leftNav: {
@@ -31,7 +33,29 @@ const useStyle = makeStyles({
         '&:hover':{
             color: "white",
         }
-    }
+    },
+    cityHero:{
+        width: "100%",
+        height: "92vh",
+    },
+    overlayText: {
+        position: "absolute",
+        zIndex: "100",
+        top: "150px",
+        left: "20px",
+        textAlign: "center",
+    },
+    homeTitle: {
+        color: "white",
+        fontWeight: "bolder",
+    },
+    heroButton: {
+        fontSize: "3.5rem",
+        borderRadius: "15px",
+        backgroundColor: "green",
+        marginTop: "2rem",
+        boxShadow: "3px 3px 3px white",
+    },
 })
 function Home() {
     const classes = useStyle();
@@ -53,6 +77,13 @@ function Home() {
                     </div>
                 </Toolbar>
             </AppBar>
+            <div style={{postion: "relative"}}>
+            <img src={city} className={classes.cityHero}/>
+            <div className={classes.overlayText}>
+                <Typography variant='h1' className={classes.homeTitle}>FIND YOUR <span style={{color: "green"}}>NEXT PROPERTY</span>  ON OUR WEBSITE</Typography>
+                <Button variant='contained' className={classes.heroButton}>SEE ALL PROPERTIES</Button>
+            </div>
+            </div>
         </>
     )
 }

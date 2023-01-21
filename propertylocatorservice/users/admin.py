6ea8bf3d6e9049d918129ef54bdd151f0bd0bcec
miguel <miguel@cinaq.com>
2023-01-21 14:47:31 +0100

    remove backend files for frontend branch

--- propertylocatorservice/users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth import get_user_model
-User = get_user_model()
-from .models import Profile
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(Profile)
